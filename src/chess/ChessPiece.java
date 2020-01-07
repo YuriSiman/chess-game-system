6fe38f156a641d6247afb87915dd0fa097232365
Yuri Siman <yurisimannogueira@gmail.com>
2020-01-07 09:36:18 -0300

    Placing piece on the board

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -8,21 +8,16 @@ public class ChessPiece extends Piece {
 	private Color color;
 
 	public ChessPiece() {
-		
+
 	}
-	
+
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
 
-
 	public Color getColor() {
 		return color;
 	}
 
-	
-	
-	
-	
 }
